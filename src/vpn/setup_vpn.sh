22134b0ee3bbf5c1299b62b52435063f52ee066a
FU Li <li@qcloudx.com>
2015-12-02 14:19:15 +0800

    add script

--- src/vpn/setup_vpn.sh
+++ src/vpn/setup_vpn.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x
 # http://blog.fens.me/vpn-pptp-client-ubuntu/
 
+serverip=54.178.183.214
+uid=vpn
+passwd=lzy20150101
+
+sudo poff myvpn
+sudo pptpsetup --create myvpn --server $serverip --username $uid --password $passwd --encrypt --start
+sudo ip route del default
+sudo ip route add default dev ppp0
+
